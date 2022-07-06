b87fab19d46ebe1599454fc994cc16a41d3824a2
Blessed Tuna <99805196+blennie@users.noreply.github.com>
2022-07-06 18:37:48 +0200

    Update log.spec.js

--- __tests__/log.spec.js
+++ __tests__/log.spec.js
@@ -5,62 +5,62 @@
  **************************************************************************************************************************************************************/
 
 
-const Log = require('../index.js');
-const Style = Log.Style;
+const Drips = require('../index.js');
+const Style = Drips.Style;
 
 
-test('Log.[type] - should begin with the right flag', () => {
+test('Drips.[type] - should begin with the right flag', () => {
 	console.log = jest.fn();
 	console.info = jest.fn();
 	console.error = jest.fn();
 
-	Log.banner('banner'); // log
-	Log.error('error');   // error
-	Log.info('info');     // info
-	Log.ok('ok');         // log
-	Log.done('done');     // log
+	Drips.banner('banner'); // log
+	Drips.error('error');   // error
+	Drips.info('info');     // info
+	Drips.ok('ok');         // log
+	Drips.done('done');     // log
 
 	expect( console.log.mock.calls.length ).toBe( 3 );
 	expect( console.info.mock.calls.length ).toBe( 1 );
 	expect( console.error.mock.calls.length ).toBe( 1 );
 
-	expect( Style.strip( console.log.mock.calls[0][0] ).startsWith( Log.flags.banner ) ).toBeTruthy();
+	expect( Style.strip( console.log.mock.calls[0][0] ).startsWith( Drips.flags.banner ) ).toBeTruthy();
 	expect( Style.strip( console.log.mock.calls[0][0] ).endsWith('banner') ).toBeTruthy();
 
-	expect( Style.strip( console.error.mock.calls[0][0] ).startsWith( Log.flags.error ) ).toBeTruthy();
+	expect( Style.strip( console.error.mock.calls[0][0] ).startsWith( Drips.flags.error ) ).toBeTruthy();
 	expect( Style.strip( console.error.mock.calls[0][0] ).endsWith('error') ).toBeTruthy();
 
-	expect( Style.strip( console.info.mock.calls[0][0] ).startsWith( Log.flags.info ) ).toBeTruthy();
+	expect( Style.strip( console.info.mock.calls[0][0] ).startsWith( Drips.flags.info ) ).toBeTruthy();
 	expect( Style.strip( console.info.mock.calls[0][0] ).endsWith('info') ).toBeTruthy();
 
-	expect( Style.strip( console.log.mock.calls[1][0] ).startsWith( Log.flags.ok ) ).toBeTruthy();
+	expect( Style.strip( console.log.mock.calls[1][0] ).startsWith( Drips.flags.ok ) ).toBeTruthy();
 	expect( Style.strip( console.log.mock.calls[1][0] ).endsWith('ok') ).toBeTruthy();
 
-	expect( Style.strip( console.log.mock.calls[2][0] ).startsWith( Log.flags.done ) ).toBeTruthy();
+	expect( Style.strip( console.log.mock.calls[2][0] ).startsWith( Drips.flags.done ) ).toBeTruthy();
 	expect( Style.strip( console.log.mock.calls[2][0] ).endsWith('done') ).toBeTruthy();
 
 });
 
 
-test('Log.verbose - should log nothing with verboseMode false', () => {
+test('Drips.verbose - should log nothing with verboseMode false', () => {
 	console.log = jest.fn();
 
-	Log.verboseMode = false;
+	Drips.verboseMode = false;
 
-	Log.verbose(`test`);
-	Log.verbose(`test2`);
+	Drips.verbose(`test`);
+	Drips.verbose(`test2`);
 
 	expect( console.log.mock.calls.length ).toBe( 0 );
 });
 
 
-test('Log.verbose - should log with verboseMode true', () => {
+test('Drips.verbose - should log with verboseMode true', () => {
 	console.log = jest.fn();
 
-	Log.verboseMode = true;
+	Drips.verboseMode = true;
 
-	Log.verbose(`test`);
-	Log.verbose(`test2`);
+	Drips.verbose(`test`);
+	Drips.verbose(`test2`);
 
 	expect( console.log.mock.calls.length ).toBe( 2 );
 
@@ -72,11 +72,11 @@ test('Log.verbose - should log with verboseMode true', () => {
 });
 
 
-test('Log.time - should log its message', () => {
+test('Drips.time - should log its message', () => {
 	console.log = jest.fn();
 
-	Log.time(`test`);
-	Log.time(`test2`);
+	Drips.time(`test`);
+	Drips.time(`test2`);
 
 	expect( console.log.mock.calls.length ).toBe( 2 );
 
@@ -85,10 +85,10 @@ test('Log.time - should log its message', () => {
 });
 
 
-test('Log.hr - should log a long line', () => {
+test('Drips.hr - should log a long line', () => {
 	console.log = jest.fn();
 
-	Log.hr( 10 );
+	Drips.hr( 10 );
 
 	expect( console.log.mock.calls.length ).toBe( 1 );
 
